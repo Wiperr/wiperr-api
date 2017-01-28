2d321bcc89af017df7a8609936564a601dce39c0
Akshay Singh <akshay.singh@hpe.com>
2017-01-28 13:30:24 +0530

    Updated DB configurations

---
+++ server/datasources.development.js
@@ -0,0 +1,13 @@
+/*
+ * HP Enterprise | TSRnD Bengaluru, India
+ * @author: Akshay Kr Singh
+ * @date:   1/28/2017
+ * @email: akshay.singh@hpe.com
+ */
+module.exports = {
+  "db": {
+    "url": "mongodb://wiperr-dev:wiperr-dev@ds155097.mlab.com:55097/wiperr-dev",
+    "name": "db",
+    "connector": "mongodb"
+  }
+};
