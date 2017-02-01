d9c518ed0b567eafa51e818f57da807607452f09
Akshay Singh <akshay.singh@hpe.com>
2017-02-01 12:48:18 +0530

    UPDATED Website UI | Added angular

---
+++ portal/src/app/index.module.js
@@ -0,0 +1,16 @@
+/* global malarkey:false, moment:false */
+
+import { config } from './index.config';
+import { routerConfig } from './index.route';
+import { runBlock } from './index.run';
+import { MainController } from './main/main.controller';
+import { ThemeMainDirective } from '../app/components/theme-main/themeMain.directive';
+
+angular.module('wiperrPortal', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'toastr'])
+  .constant('malarkey', malarkey)
+  .constant('moment', moment)
+  .config(config)
+  .config(routerConfig)
+  .run(runBlock)
+  .controller('MainController', MainController)
+  .directive('themeMain', ThemeMainDirective);
--- server/boot/root.js
+++ server/boot/root.js
@@ -3,6 +3,6 @@
 module.exports = (server) => {
   // Install a `/` route that returns server status
   let router = server.loopback.Router();
-  /*router.get('/', server.loopback.status());*/
+  router.get('/', (req, res) => res.redirect("/portal"));
   server.use(router);
 };
