82ea80bd9f161eb380094e3ccd866c06138120e0
Akshay Singh <akshay.singh@hpe.com>
2017-02-09 21:13:50 +0530

    RELEASE alpha-0.8.5

--- portal/src/app/index.config.js
+++ portal/src/app/index.config.js
@@ -1,5 +1,10 @@
-export function config ($logProvider) {
+export function config ($logProvider, toastrConfig) {
   'ngInject';
   // Enable log
   $logProvider.debugEnabled(true);
+
+  angular.extend(toastrConfig, {
+    positionClass: 'toast-top-center',
+    target: 'body'
+  });
 }
