fa5d6173012ab49094f3bde5cd5c5c026f07e4e4
Akshay Singh <akshay.singh@hpe.com>
2017-02-10 14:32:14 +0530

    RELEASE alpha-0.8.6

--- server/datasources.staging.js
+++ server/datasources.staging.js
@@ -6,10 +6,8 @@
  */
 module.exports = {
   "db": {
-    "host": "localhost",
-    "port": 27017,
-    "database": "wiperr-development",
+    "url": "mongodb://wiperr-dev:wiperr-dev@ds155097.mlab.com:55097/wiperr-dev",
     "name": "db",
-    "connector": process.env.DB || "mongodb"
-  },
+    "connector": "mongodb"
+  }
 };
